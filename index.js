507ab3713ddde2328dc129021f06b27844aa1692
Ghfewdev <few_za75@hotmail.com>
2023-10-24 16:26:02 +0700

    2

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const d = new Date();
+
 const storage = multer.diskStorage({
     destination: function(req, file, cd) {
-        return cd(null, "./public/images")
+        return cd(null, "./images")
     },
     filename: function(req, file, cd) {
-        return cd(null, `${Date.now()}_.${(file.originalname).split(".")[1]}`)
+        return cd(null, `${d.getMilliseconds()}_${(file.originalname).split(".")[0]}.${(file.originalname).split(".")[1]}`)
     }
 })
 
 const upload = multer({storage})
 
-app.post("/upload", upload.single("file"), (req, res) => {
+app.post("/upload", jsonParser, upload.single("file"), (req, res) => {
     //console.log(req.body)
-    console.log(req.file)
+    res.json({filename: req.file.filename})
+    //console.log(req.file.filename)
 })
 
 app.get("/", (req, res) => {
     res.send("API")
 })
 
-app.use(express.static("public"));
-//app.use("/images", express.static("images"))
+//app.use(express.static("public"));
+app.use("/images", express.static("images"))
 
 app.listen(3001, () => {
     console.log("node started on port 3001")
